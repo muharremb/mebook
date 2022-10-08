f4538684c398ca3d83b9011db1e8d9404183739a
muharrem <boztepe.muharrem@gmail.com>
2022-10-08 10:26:15 -0400

    database user table seed done

--- backend/app/models/user.rb
+++ backend/app/models/user.rb
@@ -1,3 +1,63 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id              :bigint           not null, primary key
+#  first_name      :string           not null
+#  last_name       :string           not null
+#  email           :string           not null
+#  password_digest :string           not null
+#  session_token   :string           not null
+#  created_at      :datetime         not null
+#  updated_at      :datetime         not null
+#
 class User < ApplicationRecord
   has_secure_password
+  validates :first_name, 
+    length: {in: 2..30}, 
+    format: { with: /\A[a-zA-Z]+\z/, message: "only allows letters" }
+      
+  validates :last_name, 
+    length: {in: 2..30}, 
+    format: { with: /\A[a-zA-Z]+\z/, message: "only allows letters" }
+
+  validates :email, 
+    uniqueness: true, 
+    length: { in: 3..255 }, 
+    format: { with: URI::MailTo::EMAIL_REGEXP }
+  
+  validates :session_token, presence: true, uniqueness: true
+  validates :password, length: { in: 6..255 }, allow_nil: true
+
+  before_validation :ensure_session_token
+
+
+  def self.find_by_credentials(email, password)
+
+    user = User.find_by(email: email)
+
+    return nil if user.nil?
+    return user.authenticate(password)
+  end
+
+  def reset_session_token!
+    # self.session_token = generate_unique_session_token
+    # save!
+    # self.session_token
+    self.update!(session_token: generate_unique_session_token)
+    session_token
+  end  
+
+  def generate_unique_session_token
+    session_token = SecureRandom.base64(16)
+    while User.exists?(session_token: session_token)
+      session_token = SecureRandom.base64(16)
+    end
+    return session_token
+  end
+
+  def ensure_session_token
+    self.session_token ||= generate_unique_session_token
+  end
+
 end
