cbc77979155ad53cdc8fe7ed4fc4f56305f08ce7
muharrem <boztepe.muharrem@gmail.com>
2022-10-13 10:16:02 -0400

    heroku app prep done

---
+++ config/routes.rb
@@ -0,0 +1,16 @@
+Rails.application.routes.draw do
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  # root "articles#index"
+
+  # post 'api/test', to: 'application#test'
+
+  namespace :api, defaults: { format: :json } do
+    resources :users, only: [:create, :show]
+    resources :posts, only: [:create, :delete]
+    resource :session, only: [:show, :create, :destroy]
+  end
+  
+  get '*path', to: "static_pages#frontend_index"
+end
